56b27d93612e6d7f8b68995d4ee6300f1eeb32e1
Kathy-Peng <136121398+Kathy-Peng@users.noreply.github.com>
2024-07-17 16:38:59 -0700

    updated download script to accommodate for various image extensions

--- download-images.py
+++ download-images.py
@@ -1,34 +1,27 @@
-import urllib.request 
-from PIL import Image 
+import urllib.request
+from PIL import Image
 import os
 import json
+import csv
+import time
+import requests
 from tqdm import tqdm
 from io import BytesIO
 from urllib.request import Request, urlopen
-import requests
-import os
 
-# if you are downloading videos, comment out the first line of code and uncomment the second line
-media_format = 'image/jpeg'
-# media_format = 'video/mp4'
-
-outdir = './downloaded-images'
-os.makedirs(outdir, exist_ok=True)
+dirname = "download"
+os.makedirs(dirname, exist_ok=True)
 
 idx = 0
-with open('sd3.jsonl', 'r') as linkfile:
-    for link in linkfile:
-        image_urls = link.split('"')
-        for image_url in image_urls:
-            if image_url == '' or image_url =='\n':
-                continue
-            print(image_url)
-            response = requests.get(image_url)
-            header = response.headers.get("Content-Type", "")
-            if header != media_format: 
-                continue
-            # change the jpg extension to mp4 to download videos
-            with open(os.path.join(outdir, f'{idx}.jpg'), "wb") as file:
-                file.write(response.content)
-            idx += 1 
-        
+with open("links.jsonl", "r") as file:
+    for link in tqdm(file):
+        link = link.strip('"')
+        response = requests.get(link)
+        header = response.headers.get("Content-Type", "")
+        type = header.split("/")[0]
+        extension = header.split("/")[1]
+        if type != "image":
+            continue
+        with open(os.path.join(dirname, f"{idx}.{extension}"), "wb") as file:
+            file.write(response.content)
+        idx += 1
